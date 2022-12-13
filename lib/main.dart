b57c00876564a63816c084fb86dbcba7cc00d9cd
Marcelo Antonio <inho.menezes2011@gmail.com>
2022-12-12 23:04:40 -0300

    Starting initial UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ void main() {
   runApp(const MyApp());
 }
 
+// Testing layout before getting pokemons through API
+final List<String> pokemons = <String>['Bulbassaur', 'Pikachu', 'Bayleef', 'Lapras', 'Charmander', 'Squirtle', 'Charmeleon' ,'Infernape', 'Mew'];
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -16,8 +19,24 @@ class MyApp extends StatelessWidget {
           title: const Text('Pokedex'),
         ),
 
-        body: const Center(
-          child: Text('Place pokemons here'),
+        body: GridView.builder(
+          padding: const EdgeInsets.all(10),
+          itemCount: pokemons.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 200,
+              color: Colors.amber[100],
+              child: Center(
+                child: Text('#$index, ${pokemons[index]}')
+              ),
+            );
+          },
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 13.0,
+            crossAxisSpacing: 13.0,
+            childAspectRatio: 1/2
+            ),
         ),
       ),
     );
